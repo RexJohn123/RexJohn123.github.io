e56a2c31b4f9faf9fcc043e004bedfc8b93b35b0
RexJohn123 <87732246+RexJohn123@users.noreply.github.com>
2021-12-08 12:55:56 -0500

    update 12/8/21

--- js/java.js
+++ js/java.js
@@ -34,3 +34,22 @@ window.onclick = function(event) {
   }
 }
 */
+
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById('myButton');
+
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function (){
+  modal.style.display = "block";
+}
+
+span.onclick = function(){
+  modal.style.display = "none";
+}
+window.onclick = function(){
+  if (event.target == modal){
+    modal.style.display = "none";
+  }
+}
